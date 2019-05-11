4d03a8c65bfcd17d9b290dee8b511ff284c6409a
Guilherme Prado <guilhermegravinap@gmail.com>
2019-05-11 18:15:50 -0300

    unfinshed dumb version of doctor

--- src/components/Doctor.java
+++ src/components/Doctor.java
@@ -4,24 +4,34 @@ import components.interfaces.IResponder;
 import data.interfaces.ITableProducer;
 
 public class Doctor implements IDoctor{
-
+	private IResponder responder;
+	private ITableProducer producer;
+	
 	@Override
 	public void connect(IResponder responder) {
-		// TODO Auto-generated method stub
-		
+		this.responder = responder;		
 	}
 
 
 	@Override
 	public void connect(ITableProducer producer) {
-		// TODO Auto-generated method stub
-		
+		this.producer = producer;
 	}
 	
 	@Override
 	public void startInterview() {
-		// TODO Auto-generated method stub
+		//IDEA _ mais burro possivel: contruir o hashmap perguntando TUDO
+		// tirar o campo diagonistico, comparar, dar o diagnotico
+		String[] attributes = this.producer.requestAttributes();
+		String diagnotic = null;
 		
+		if (responder != null) {
+			for(int i = 0;i<attributes.length; i++) {
+				this.responder.ask(attributes[i]);
+			}
+		} else {
+			System.out.println("I Can't start an interview without a Responder :(");
+		}
 	}
 
 
