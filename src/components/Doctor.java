2c79dc13426dc11bc067fdea03278c7240d99ce5
Guilherme Prado <guilhermegravinap@gmail.com>
2019-06-08 17:50:30 -0300

    implemented solutions for equal cases leading to dif diagnostics

--- src/components/Doctor.java
+++ src/components/Doctor.java
@@ -1,12 +1,12 @@
 package components;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 import components.interfaces.IDoctor;
-import components.interfaces.IProbability;
 import components.interfaces.IResponder;
 import data.interfaces.ISmartDataProducer;
 import data.interfaces.ITableProducer;
@@ -41,21 +41,40 @@ public class Doctor implements IDoctor{
 		String[] attributes = this.producer.requestAttributes();
 		String[] symptons = Arrays.copyOfRange(attributes, 0, attributes.length-1);
 		String diagnostic = "not sure";
-
-		
-
+		int questionAsked = 0;
+		DecimalFormat df2 = new DecimalFormat("#.##");
+		boolean oddCase = false; 
 
 		//criar condicao para parar o loop
-		while(calculateProbability() < 0.80) {
-			String question = sProducer.bestAttribute(symptons);
+		while(calculateProbability() < 0.80 && questionAsked < (attributes.length-1)) {
+			String question = sProducer.bestAttribute(symptons);	
 			String response;
-			System.out.println("Doctor: Do you have " + question);
+			// caso tenha duas doenças iguais
+			if(question == null) {
+				oddCase = true;
+				break;
+			}
+			System.out.println("Doctor: Do you have " + question + "?");
 			response = responder.ask(question);
 			sProducer.removeHash(question, response);		
+			questionAsked++;
 		}
+		
 		diagnostic = bestGuess();
 		boolean result = responder.finalAnswer(diagnostic);
-		System.out.println("U probably have: " + diagnostic);
+		
+		//case 2 or more have the same symptons with different diseases
+		if (oddCase == true) {
+			System.out.println("We are facing an odd case, your disease can be the following things: ");
+			for(String disease : sProducer.outComeMap().keySet())
+				System.out.println(disease);
+			System.out.print("But my guess is: ");
+		} else {
+			System.out.print("U probably have: ");
+		}
+		
+		System.out.println(diagnostic + " (probability: " + 
+				df2.format(calculateProbability()*100) + "%)");
 		System.out.println("Correct answer? " + ((result) ? "I'm right!" : "I'm wrong"));
 	}
 	private double calculateProbability() {
